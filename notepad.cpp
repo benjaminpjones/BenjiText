c402194978f8cf31546e9ab94bb03b860964193c
Benjamin Jones <BenjaminPaikJones@gmail.com>
2018-01-05 23:40:43 -0600

    First Commit

---
+++ notepad.cpp
@@ -0,0 +1,15 @@
+#include "notepad.h"
+#include "ui_notepad.h"
+
+Notepad::Notepad(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::Notepad)
+{
+    ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+}
+
+Notepad::~Notepad()
+{
+    delete ui;
+}
